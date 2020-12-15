51ed1e4bd8e6f85f1f4010c68e9863b6057e3762
Joan <69653003+Johan769@users.noreply.github.com>
2020-12-15 17:07:50 -0500

    Subiendo los cambios de el algoritmo de dijkstra

--- js/dijkstra.js
+++ js/dijkstra.js
@@ -3,13 +3,43 @@ class Graph {
     this.nodes = [];
     this.adjacentList = {};
   }
-  addNode() {
+  addNode(node) {
     this.nodes.push(node);
     this.adjacentList[node] = [];
   }
   addEdge(node1, node2, weight) {
     this.adjacentList[node1].push({node: node2, weight: weight});
     this.adjacentList[node2].push({node: node1, weight: weight});
+  }
+  dijkstra(startNode, endNode) {
+    let times = {}, backtrace = {};
+    let pq = new PriorityQueue();
+    times[startNode] = 0;
+    this.nodes.forEach(node => {
+      if (node !== startNode) {
+        times[node] = Infinity;
+      }
+    });
+    pq.enqueue([startNode, 0]);
+    while (!pq.isEmpty()) {
+      let shortestStep = pq.dequeue();
+      let currentNode = shortestStep[0];
+      this.adjacentList[currentNode].forEach(neighbor => {
+        let time = times[currentNode] + neighbor.weight;
+        if (time < times[neighbor.node]) {
+          times[neighbor.node] = time;
+          backtrace[neighbor.node] = currentNode;
+          pq.enqueue([neighbor.node, time]);
+        }
+      });
+    }
+    let path = [endNode];
+    let lastStep = endNode;
+    while (lastStep !== startNode) {
+      path.unshift(backtrace[lastStep]);
+      lastStep = backtrace[lastStep];
+    }
+    return `El camino ${path} con una distancia de ${times[endNode]}`;
   }
 }
 
@@ -22,7 +52,7 @@ class PriorityQueue {
       this.collection.push(element);
     } else {
       let added = false;
-      for (let i = 0; i < this.collection.length; ++i) {
+      for (let i = 1; i < this.collection.length; ++i) {
         if (element[1] < this.collection[i-1][1]) {
           this.collection.splice(i-1, 0, element);
           added = true;
@@ -43,33 +73,13 @@ class PriorityQueue {
   }
 }
 
-const Dijkstra = (startNode, endNode) => {
-  let times = {}, backtrace = {};
-  let pq = new PriorityQueue();
-  times[startNode] = 0;
-  this.nodes.forEach(node => {
-    if (node !== startNode) {
-      times[node] = Infinity;
-    }
-  });
-  pq.enqueue([startNode, 0]);
-  while (!pq.isEmpty()) {
-    let shortestStep = pq.dequeue();
-    let currentNode = shortestStep[0];
-    this.adjacentList[currentNode].forEach(neighbor => {
-      let time = times[currentNode] + neighbor.weight;
-      if (time < times[neighbor.node]) {
-        times[neighbor.node] = time;
-        backtrace[neighbor.node] = currentNode;
-        pq.enqueue([neighbor.node, time]);
-      }
-    });
-  }
-  let path = [endNode];
-  let lastStep = endNode;
-  while (lastStep !== startNode) {
-    path.unshift(backtrace[lastStep]);
-    lastStep = backtrace[lastStep];
-  }
-  return `El camino ${path} con un tiempo de ${times[endNode]}`;
-}
+const Grafo = new Graph();
+console.log(Grafo);
+Grafo.addNode("joan");
+Grafo.addNode("estefany");
+Grafo.addNode("lisa");
+Grafo.addEdge("joan", "estefany", 10);
+Grafo.addEdge("joan", "lisa", 2);
+Grafo.addEdge("estefany", "lisa", 5);
+console.log(Grafo);
+console.log(Grafo.dijkstra("lisa", "estefany"));
